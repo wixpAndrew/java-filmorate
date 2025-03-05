44bf48ae6a938dd477bb92ce0feb2d2d86870d35
User Andrey <hommander3002@gmail.com>
2025-03-05 19:44:57 +0300

    начало

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Point.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.*;
+
+@ToString
+@Value
+@Builder(toBuilder = true)
+public class Point {
+    int x;
+    int y;
+    String description;
+
+}
