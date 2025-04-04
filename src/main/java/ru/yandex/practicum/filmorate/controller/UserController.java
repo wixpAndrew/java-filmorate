61269b94afdba4a036ed6ff973b2c155f564e0a6
User Andrey <hommander3002@gmail.com>
2025-04-04 22:34:46 +0300

    dff

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNullFields;
 import org.springframework.web.bind.annotation.*;
 
 import ru.yandex.practicum.filmorate.exception.ValidationException;
@@ -16,6 +17,7 @@ import ru.yandex.practicum.filmorate.exception.DuplicatedDataException;
 
 @RestController
 @RequestMapping("/users")
+
 public class UserController {
 
     private final Map<Integer, User> users = new HashMap<>();
